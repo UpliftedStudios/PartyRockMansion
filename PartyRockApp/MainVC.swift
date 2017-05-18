eb7cf02db334499ce7f8c419a9908221dfe982a2
Uplifted Studios <raphaelhidalgo3@gmail.com>
2017-05-17 19:59:56 -0700

    UI Complete

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Raphael M. Hidalgo on 5/11/17.
+//  Copyright © 2017 UpliftedStudios. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
